25930c282b2037af3a2ff499996ca386be56ba87
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-10-02 19:46:36 +0600

    button added

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -4,6 +4,13 @@ const Break = () => {
     return (
         <div className='break-container'>
             <h3>Add A Break</h3>
+            <div className='breaks'>
+            <button>10s</button>
+            <button>20s</button>
+            <button>30s</button>
+            <button>40s</button>
+            <button>50s</button>
+            </div>
         </div>
     );
 };
