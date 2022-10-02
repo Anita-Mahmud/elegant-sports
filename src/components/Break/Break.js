10e731da7898056edabc4ad5a78f876e95e9c8c0
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-10-02 14:32:04 +0600

    info added

---
+++ src/components/Break/Break.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Break.css'
+const Break = () => {
+    return (
+        <div className='break-container'>
+            <h3>Add A Break</h3>
+        </div>
+    );
+};
+
+export default Break;
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Container.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDumbbell} from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot} from '@fortawesome/free-solid-svg-icons'
 import Cards from '../Cards/Cards';
+import Break from '../Break/Break';
 
 const Container = () => {
     return (
@@ -13,7 +15,14 @@ const Container = () => {
            <Cards></Cards>
         </div>
         <div className='info-Container'>
-              yup  
+             <h2>Anita Mahmud Jhara</h2>
+             <h4><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon><span>Dhaka,Bangladesh</span></h4>
+             <div className='infos'>
+                <h4>75kg</h4>
+                <h4>4.11'</h4>
+                <h4>27yrs</h4>
+             </div>
+             <Break></Break>
         </div>
         </div>
     );
