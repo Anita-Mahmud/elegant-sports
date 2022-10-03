a42652f4df095df7221ce0789d18f2db2c2b615b
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-10-03 19:07:53 +0600

    responsive

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Break.css'
 const Break = () => {
+    
     return (
         <div className='break-container'>
             <h3>Add A Break</h3>
             <div className='breaks'>
-            <button>10s</button>
-            <button>20s</button>
-            <button>30s</button>
-            <button>40s</button>
-            <button>50s</button>
+            <button id="1">10s</button>
+            <button id="2">20s</button>
+            <button id="3">30s</button>
+            <button id="4">40s</button>
+            <button id="5">50s</button>
             </div>
         </div>
     );
